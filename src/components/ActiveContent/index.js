b186ae6257220ad443a3053947fc48b794b54c83
eslam3bed <e.eslam3bed@gmail.com>
2018-10-01 17:31:37 +0300

    remvoing the useless import

--- src/components/ActiveContent/index.js
+++ src/components/ActiveContent/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import Dashboard from '../Dashboard'
-import NewProductDetailes from '../ProductDetails'
 
 import './style.css'
 export default ({ children, ...props }) => (
