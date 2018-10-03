d9ca581886dad4086e6b286b51311787a5bc6d8d
eslam3bed <e.eslam3bed@gmail.com>
2018-10-03 15:19:09 +0300

    creating and abstracing comopnent from the two new tabs in the new product page(order bump,payments)

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,8 +2,10 @@ import * as boxes from './boxes'
 import * as inputs from './Inputs'
 import * as logos from './logos'
 import * as search from './search'
+import * as titles from './Titles'
+import * as cards from './Cards'
 
 
 
 // console.log({ inputs})
-export default { ...boxes, ...inputs, ...logos, ...search }
+export default { ...boxes, ...inputs, ...logos, ...search,...titles ,...cards}
