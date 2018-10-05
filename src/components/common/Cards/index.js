89fd0c3dfd34948b80e160b8582344614cb270ea
eslam3bed <e.eslam3bed@gmail.com>
2018-10-05 04:05:05 +0300

    adding the building blocks components for the product page and for future resuable

--- src/components/common/Cards/index.js
+++ src/components/common/Cards/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './style.css'
 export const MiniCard = ({ imgSrc, ...props }) => {
 
-console.log(imgSrc)
+    console.log(imgSrc)
 
     return (
         <img
@@ -12,3 +12,25 @@ console.log(imgSrc)
             className='small-solid-card white-color' />
     )
 }
+export const ProductAvatar = ({ imageSrc, name }) => (
+    imageSrc ?
+        <img src={imageSrc} alt={name} className='product-image-avatar'></img>
+        :
+        <span className='product-name-avatar'>{name.splice(2)}</span>
+)
+
+export const ProductCard = ({ name,currancy, monthlyProfite, price, onEdit, onExplore, onDelete, ...props }) => (
+    <div className='product-card-container'>
+        <div className='product-avatar-holder'>
+            <ProductAvatar {...props}/>
+            <span className='product-profit-holder'>{monthlyProfite}</span>
+            <span className='product-price-holder'>{`${currancy+price}`}</span>
+        </div>
+    </div>
+        )
+
+export const NewProductCard = props =>(
+    <div className='product-card-container new-product-card'>
+        +New Product
+    </div>
+)
