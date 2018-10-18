c4cb2e2975d8a250fc6ced5244a6dec6cf00c822
eslam3bed <e.eslam3bed@gmail.com>
2018-10-18 21:15:58 +0300

    adding few constant Types to make it globly uniqe

---
+++ src/constantsTypes/index.js
@@ -0,0 +1,9 @@
+export const SIGN_UP = Symbol('SIGN_UP')
+export const LOGIN = Symbol('LOGIN')
+
+export const SIGNING_UP = Symbol('SIGNING_UP')
+export const SIGN_UP_SUCCESS = Symbol('SIGN_UP_SUCCESS')
+export const SIGN_UP_FAILD = Symbol('SIGN_UP_FAILD')
+export const LOGGING_IN = Symbol('LOGGING_IN')
+export const LOGIN_SUCCESS = Symbol('LOGIN_SUCCESS')
+export const LOGIN_FAILD = Symbol('LOGIN_FAILD')
