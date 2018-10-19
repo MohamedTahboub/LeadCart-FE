942081c935257e77655b430edc3b8a17e049bbf8
eslam3bed <e.eslam3bed@gmail.com>
2018-10-19 16:04:19 +0300

    adding the signup middleware to handle the signup process

--- src/middlewares/signup.js
+++ src/middlewares/signup.js
@@ -1,21 +1,57 @@
-import { SIGN_UP, SIGN_UP_SUCCESS } from 'constantsTypes'
-import user from '../reducers/user';
 
-export default ({ dispatch, setState }) => next => ({ type, payload }) => {
+import apiRequest from './helpers/apiRequest'
+import { Rules, Vaidator } from './helpers/validators'
+import { SIGN_UP_SUBMIT, SIGN_UP_ON_FIRST_STAGE_SUBMITION } from '../constantsTypes';
 
-    if (type === SIGN_UP) {
-        // validate the new user inputs 
-        // isSignUpFormValid(user) && make arequest to sign a user Up
+import {
+    signUpFirstStageComplete,
+    signUpInvalidFields,
+    signUpSuccess,
+    signUpFaild
+} from 'actions/signup'
 
-        // api request to signup user and when success it will dispatch assccess signup or failur 
-        // dispatch(requestAPI({
-        //     method : 'post',
-        //     body: user,
-        //     uri : '/api/signup'
-        // } ,{ onSignUpSuccess , onSignUpFaild} ))
-    } else {
+export default ({ dispatch }) => next => ({ type, payload }) => {
+
+    if (type === SIGN_UP_ON_FIRST_STAGE_SUBMITION) {
+        const rules = {
+            'username': [Rules.isRequired],
+            'email': [Rules.email],
+            'company': [Rules.isRequired],
+            'password': [Rules.isRequired],
+            'subdomain': [Rules.subdomain]
+        }
+
+        const isInvalid = Vaidator(payload, rules)
+
+        if (isInvalid)
+            dispatch(signUpInvalidFields(isInvalid))
+        else
+            dispatch(signUpFirstStageComplete(payload))
 
 
-    }
-    next({ type, payload })
+    } else if (type === SIGN_UP_SUBMIT) {
+        const rules = { 'password': [Rules.isRequired] }
+
+        const isInvalid = Vaidator(payload, rules)
+
+        if (isInvalid)
+            dispatch(signUpInvalidFields(isInvalid))
+        else {
+            apiRequest({
+                method: 'POST',
+                body: payload,
+                uri: '/users/signup'
+            })
+                .then(({ status, ...response }) =>
+                    status ?
+                        dispatch(signUpSuccess(response))
+                        :
+                        dispatch(signUpFaild(response)))
+                .catch(err => dispatch(signUpFaild(err.message)))
+
+        }
+
+    } else return next({ type, payload })
+
+
 }
