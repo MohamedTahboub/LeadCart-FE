daaea3445e7367f980998c6af700403058acd1f5
eslam3bed <e.eslam3bed@gmail.com>
2018-10-22 11:06:03 +0300

    creating simple,small validation module to validate the application fields

---
+++ src/middlewares/helpers/apiRequest.js
@@ -0,0 +1,18 @@
+export default async ({ method = 'GET', body = {}, uri }) => {
+    try {
+        const options = {
+            method,
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            credentials: 'same-origin',
+            body: JSON.stringify(body)
+        }
+        const response = await fetch(uri, options);
+
+        return response
+    } catch (err) {
+        return { status: false, message: err.message }
+    }
+}
