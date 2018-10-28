c252837aeb095efaf8d187b677470906af637257
eslam3bed <e.eslam3bed@gmail.com>
2018-10-28 16:08:38 +0200

    redirect to home if the user exist in the localStorage

--- src/middlewares/login.js
+++ src/middlewares/login.js
@@ -1,25 +1,25 @@
-import apiRequest from './helpers/apiRequest'
 import { LOGIN } from 'constantsTypes';
+import { apiRequest } from 'actions/apiRequest';
 
 import {
-    loginSuccess,
-    loginFaild
-} from 'actions/login'
+  loginSuccess,
+  loginFaild
+} from 'actions/login';
 
-export default ({ dispatch }) => next => action => {
+export default ({ dispatch }) => (next) => (action) => {
+  if (action.type !== LOGIN) return next(action);
 
-    if (action.type !== LOGIN) return next(action)
-
-    apiRequest({
-        method: 'POST',
-        body: action.payload,
-        uri: '/api/users/login'
-    })
-        .then(({ success, message , data }) => {
-            return success ?
-                dispatch(loginSuccess(data))
-                :
-                dispatch(loginFaild(message))
-        })
-        .catch(err => dispatch(loginFaild('Something gone wrong,please try again later ')))
-}
+  dispatch(apiRequest({
+    options: {
+      method: 'POST',
+      body: action.payload,
+      uri: '/api/users/login'
+    },
+    onSuccess: loginSuccess,
+    onFaild: loginFaild
+  }));
+  // .then(({ success, message, data }) => (success
+  //   ? dispatch(loginSuccess(data))
+  //   : dispatch(loginFaild(message))))
+  // .catch((err) => dispatch(loginFaild('Something gone wrong,please try again later ')));
+};
