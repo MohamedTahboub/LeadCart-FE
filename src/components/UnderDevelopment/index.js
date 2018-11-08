c762ef92cd03d1ce05011807b9dacec86fb809fe
eslam3bed <e.eslam3bed@gmail.com>
2018-11-08 23:12:49 +0200

    adding temp page for the inpreogress features

---
+++ src/components/UnderDevelopment/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+export default (props) => (
+  <div className='underdevelopment-container'>
+    <span className='underdevelopment-head-message'>&lt;Sorry /&gt;</span>
+    <span className='underdevelopment-notes'>
+            we are working so hard to get this feature done for you
+    </span>
+    <span className='underdevelopment-notes underdevelopment-thanks'>
+            Thank you for your patient
+    </span>
+  </div>
+);
