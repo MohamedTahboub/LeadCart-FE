fa1d692d9373c3cba8dba7ebc915f941cf418066
eslam3bed <e.eslam3bed@gmail.com>
2018-11-09 19:19:38 +0200

    creating simple server to serve the react app

--- tempServer/server.js
+++ tempServer/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/../build')));
 
 
 // match one above, send back React's index.html file.
