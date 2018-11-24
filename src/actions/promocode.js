44fc662056128659b40472f1eb23e2410920082d
esmat.nawahda <esmat.nawahda@gmail.com>
2018-11-24 19:26:25 +0200

    ....

--- src/actions/promocode.js
+++ src/actions/promocode.js
@@ -1,19 +1,8 @@
-import {
-  PROMO_CODE_ACTIVATE,
-  PROMO_CODE_ACTIVATE_SUCCESS,
-  PROMO_CODE_ACTIVATE_FAILD
-} from 'constantsTypes';
+import { PROMO_CODE_ACTIVATE } from '../constantsTypes';
 
 
-export const activatePromocode = (code) => ({
-  type: PROMO_CODE_ACTIVATE,
-  payload: code
-});
-export const activatePromocodeSuccess = (code) => ({
-  type: PROMO_CODE_ACTIVATE_SUCCESS,
-  payload: code
-});
-export const activatePromocodeFaild = (message) => ({
-  type: PROMO_CODE_ACTIVATE_FAILD,
-  payload: message
-});
+
+export const activatePromoCode = code =>({
+    type : PROMO_CODE_ACTIVATE,
+    payload:code
+})
