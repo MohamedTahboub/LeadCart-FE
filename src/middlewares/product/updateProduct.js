b204bba63f188203eae58217ce563c3a22ff23a1
eslam3bed <e.eslam3bed@gmail.com>
2018-12-16 08:57:06 +0200

    formating the product update data payload to suit the BackENd payload accepted model

--- src/middlewares/product/updateProduct.js
+++ src/middlewares/product/updateProduct.js
@@ -3,11 +3,30 @@ import { UPDATE_PRODUCT } from 'constantsTypes';
 import { productUpdatedSuccessfuly, productUpdatedFaild } from 'actions/product';
 import { apiRequest } from 'actions/apiRequest';
 
+import * as modles from '../helpers/models';
+import modeler from '../helpers/modeler';
+
 export default ({ dispatch, getState }) => (next) => (action) => {
   if (action.type !== UPDATE_PRODUCT) return next(action);
 
-  const { product } = getState()
-  console.log(product)
+  const { product } = getState();
+  // console.log(product);
+  // const { _id: productId, ...details } = mandatoryDetails;
+  // console.log('============', JSON.stringify(getProductFromat(product), null, 2));
+  dispatch(apiRequest({
+    options: {
+      method: 'put',
+      body: getProductFromat(product),
+      uri: '/api/products',
+      contentType: 'json'
+    },
+    onSuccess: productUpdatedSuccessfuly,
+    onFaild: productUpdatedFaild
+  }));
+  // restore the application stored data in the loaclStorage
+};
+
+function getProductFromat (product) {
   const {
     checkoutPage,
     mandatoryDetails,
@@ -17,28 +36,34 @@ export default ({ dispatch, getState }) => (next) => (action) => {
     settings,
     offer,
     thankYouPage
-  } = product
-  const { _id: productId, ...details } = mandatoryDetails
-  console.log('============', JSON.stringify({
+  } = Object.keys(modles).reduce((final, key) => {
+    console.log(product[key]);
+    final[key] = modeler(product[key], modles[key]);
+    return final;
+  }, {});
+
+  const { _id: productId, ...details } = mandatoryDetails;
+
+  return {
     productId,
     details: {
       ...details,
       checkoutPage: { ...checkoutPage, ...boosters },
-      payment,
+      payment: { ...payment, methods: ['Stripe'] },
       offer,
       settings
     }
-  }
-    , null, 2))
-  // dispatch(apiRequest({
-  //   options: {
-  //     method: 'put',
-  //     body: action.payload,
-  //     uri: '/api/products',
-  //     contentType: 'json'
-  //   },
-  //   onSuccess: productUpdatedSuccessfuly,
-  //   onFaild: productUpdatedFaild
-  // }));
-  // restore the application stored data in the loaclStorage
-};
+  };
+}
+
+
+// const payload = {
+//   settings: modeler(productDetails.settings, settings),
+//   checkoutPage: modeler(productDetails.checkoutPage, checkoutPage),
+//   mandatoryDetails: modeler(productDetails, mandatoryDetails),
+//   payment: modeler(productDetails.payment, payment),
+//   fullfillment: modeler(productDetails, fullfillment),
+//   boosters: modeler(productDetails.checkoutPage, boosters),
+//   offer: modeler(productDetails.offer, offer),
+//   thankYouPage: modeler(productDetails.payment, thankYouPage)
+// };
