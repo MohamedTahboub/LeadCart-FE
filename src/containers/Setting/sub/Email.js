a9aee1efca2f86a7aefbbf4814b84aa0baccad5d
eslam3bed <e.eslam3bed@gmail.com>
2019-01-16 10:02:21 +0200

    adding the loading spinner to the test s btns when clicked

--- src/containers/Setting/sub/Email.js
+++ src/containers/Setting/sub/Email.js
@@ -15,7 +15,7 @@ const EmailTestButton = ({
   return (
     <SmallButton
       classes={classes}
-      onClick={onClick}
+      onClick={onClick.bind(this, type)}
     >
       Test
     </SmallButton>
@@ -35,9 +35,16 @@ class Email extends Component {
     });
   }
 
+  onEmailTest = (type) => {
+    this.setState({
+      testing: true,
+      emailTestType: type
+    });
+  }
+
   render () {
     const {
-      state: { showEmailFooterModal, emailTestType }
+      state: { showEmailFooterModal, testing, emailTestType }
     } = this;
     return (
       <React.Fragment>
@@ -59,7 +66,7 @@ class Email extends Component {
               />
             </InputRow.Note>
           </InputRow>
-          <InputRow margin='30'>
+          {/* <InputRow margin='30'>
             <InputRow.Label
               notes='Send a receipt to your customers each time they are billed for their subscription.'
             >
@@ -74,7 +81,7 @@ class Email extends Component {
               Enable Self-Cancellation Link
             </InputRow.Label>
             <InputRow.SwitchInput value />
-          </InputRow>
+    </InputRow> */}
         </MainBlock>
 
         <MainBlock title='Orders Emails'>
@@ -84,7 +91,8 @@ class Email extends Component {
               content='Test the system email sent to customer when any new order is placed.'
             >
               <EmailTestButton
-                type=''
+                loading={testing}
+                type='orderRecipt'
                 testingType={emailTestType}
                 onClick={this.onEmailTest}
               />
@@ -96,7 +104,8 @@ class Email extends Component {
               content='Test the system email sent to customer when any order item is refunded.'
             >
               <EmailTestButton
-                type=''
+                loading={testing}
+                type='refundOrder'
                 testingType={emailTestType}
                 onClick={this.onEmailTest}
               />
@@ -108,7 +117,8 @@ class Email extends Component {
               content='Test the system email sent to customer when any recurring subscription is canceled.'
             >
               <EmailTestButton
-                type=''
+                loading={testing}
+                type='cancelSubscription'
                 testingType={emailTestType}
                 onClick={this.onEmailTest}
               />
@@ -120,7 +130,8 @@ class Email extends Component {
               content='Test the system email sent to customer when any subscription charge is refunded.'
             >
               <EmailTestButton
-                type=''
+                loading={testing}
+                type='refundSubscription'
                 testingType={emailTestType}
                 onClick={this.onEmailTest}
               />
@@ -132,7 +143,8 @@ class Email extends Component {
               content='Test the email sent to customers when their subscription charges.'
             >
               <EmailTestButton
-                type=''
+                loading={testing}
+                type='subscriptionChargeReceipt'
                 testingType={emailTestType}
                 onClick={this.onEmailTest}
               />
@@ -146,7 +158,8 @@ class Email extends Component {
               content='Test the default dunning email sent to customer when any subscription payment fails. This can be overridden by customizing the individual dunning steps below.'
             >
               <EmailTestButton
-                type=''
+                loading={testing}
+                type='defaultDunning'
                 testingType={emailTestType}
                 onClick={this.onEmailTest}
               />
@@ -158,7 +171,8 @@ class Email extends Component {
               content='Test the dunning email sent to customer when any subscription payment fails for the first time.'
             >
               <EmailTestButton
-                type=''
+                loading={testing}
+                type='dunning1'
                 testingType={emailTestType}
                 onClick={this.onEmailTest}
               />
@@ -170,7 +184,8 @@ class Email extends Component {
               content='Test the dunning email sent to customer when any subscription payment fails for the 2nd time.'
             >
               <EmailTestButton
-                type=''
+                loading={testing}
+                type='dunning2'
                 testingType={emailTestType}
                 onClick={this.onEmailTest}
               />
@@ -182,7 +197,8 @@ class Email extends Component {
               content='Test the dunning email sent to customer when any subscription payment fails for the 3rd time.'
             >
               <EmailTestButton
-                type=''
+                loading={testing}
+                type='dunning3'
                 testingType={emailTestType}
                 onClick={this.onEmailTest}
               />
@@ -194,7 +210,8 @@ class Email extends Component {
               content='Test the dunning email sent to customer when any subscription payment fails for the 4th time.'
             >
               <EmailTestButton
-                type=''
+                loading={testing}
+                type='dunning4'
                 testingType={emailTestType}
                 onClick={this.onEmailTest}
               />
