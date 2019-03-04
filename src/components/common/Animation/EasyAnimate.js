a1c947c81e8f5b581dfb3b4fbcbaaeab35a2751c
eslam3bed <e.eslam3bed@gmail.com>
2019-03-04 13:14:04 +0200

    animate the modal component

---
+++ src/components/common/Animation/EasyAnimate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
+
+
+export default ({ children, className }) => {
+  const props = useSpring({
+    to: async (next) => {
+      await next({ opacity: 1, color: 'rgba(0, 0, 0, 0.4)' });
+    },
+    from: { opacity: 0, color: '#fff' }
+  });
+
+
+  return (<animated.div style={props} className={className}>{children}</animated.div>);
+};
