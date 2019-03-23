ed358381ae32af30487e244106be0f8c7df02302
eslam3bed <e.eslam3bed@gmail.com>
2019-03-23 19:17:26 +0300

    adjust the intercom appeariance to not conflict with the opened modal, fixies few styles

--- src/components/Modals/SlideModal.js
+++ src/components/Modals/SlideModal.js
@@ -1,32 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { showIntercomIcon } from 'libs';
 import { EasyAnimate, SlidingAnimation } from '../common/Animation';
 
 import './slideModal.css';
 export default ({
-  children, onClose, className = '', header, footer, isVisible
+  children,
+  onClose,
+  className = '',
+  contentClassName = '',
+  bodyClassName = '',
+  header,
+  footer,
+  isVisible
 }) => {
-  setTimeout(() => {
-    showIntercomIcon(!isVisible);
-  }, 200);
-
+  
+  showIntercomIcon(!isVisible);
   return (isVisible
     ? (
       <EasyAnimate className={`slide-modal-container ${className}`}>
         <div onClick={onClose} className='slide-modal-background' role='presentation' />
-        <SlidingAnimation type='horizontal' units={1000} className='slide-modal-content'>
-          <div className='slide-modal-header'>
+        <SlidingAnimation type='horizontal' units={1000} className={`slide-modal-content ${contentClassName}`}>
+          {header && (<div className='slide-modal-header'>
             <span onClick={onClose} className='slide-modal-close-btn' role='presentation'>
               <i className='fas fa-chevron-circle-left' />
             </span>
             {header}
           </div>
-          <div className='slide-modal-body'>
+          )}
+          <div className={`slide-modal-body ${bodyClassName}`}>
             {children}
           </div>
-          <div className='slide-modal-footer'>
+          {footer && (<div className='slide-modal-footer'>
             {footer}
           </div>
+          )}
         </SlidingAnimation>
       </EasyAnimate>
     )
