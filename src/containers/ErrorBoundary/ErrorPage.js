029beea14d9ab525a2f145643b93544273e1291c
eslam3bed <e.eslam3bed@gmail.com>
2019-04-21 18:55:20 +0300

    adding error page to handle errors ui on application failur , simple ui as afallback

---
+++ src/containers/ErrorBoundary/ErrorPage.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import './style.css';
+const ErrorPage = () => (
+  <div className='error-page-container'>
+    <button className='error-code'>
+      <div className='number five'>
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+        <div className='cell empty' />
+        <div className='cell empty' />
+
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+
+        <div className='cell empty' />
+        <div className='cell empty' />
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+      </div>
+      <div className='number zero'>
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+      </div>
+      <div className='number zero'>
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+
+        <div className='cell empty' />
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell filled'><div className='bug' /></div>
+        <div className='cell empty' />
+      </div>
+    </button>
+  </div>
+);
+
+export default ErrorPage;
