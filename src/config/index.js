3445cd72517d7a4ce2f637c5e9703c85499d3410
eslam3bed <e.eslam3bed@gmail.com>
2019-04-25 11:24:50 +0300

    fix the env values to node env

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 import config from 'react-global-configuration';
 
-const env = process.env.ENVIRONMENT;
+const env = process.env.NODE_ENV;
+
 config.set({
   PORT: process.env.ENVIRONMENT === 'production' ? 3000 : 3001,
   env: process.env.ENVIRONMENT,
@@ -9,12 +10,12 @@ config.set({
   SITE_DOMAIN: env === 'dev' ? 'http://localhost:3000' : '',
   S3_DIR: ''
 });
-
 export const paymentMethodsLinks = {
-  dev: { // ca_C82XtIMbphaPO4JDiO1TJretDIMhEi3o
+  development: { // ca_C82XtIMbphaPO4JDiO1TJretDIMhEi3o
     stripe: 'https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_C82XtIMbphaPO4JDiO1TJretDIMhEi3o&scope=read_write',
   },
   production: {
     stripe: 'https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_C82X3QxgSvqB2WGT6tMgfEV7PCSehFW4&scope=read_write'
   }
-}[env || 'dev'];
+}[env];
+
