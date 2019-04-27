0a940a6180cb46ee9eba8e4b36a32f7637f1fe5e
eslam3bed <e.eslam3bed@gmail.com>
2019-04-27 05:24:59 +0300

    fix the user package type

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -42,7 +42,14 @@ export default (state = initialState, { type, payload }) => {
   switch (type) {
   case SIGN_UP_SUCCESS: return { singupSuccess: true };
   case SIGN_UP_FAILED: return { singupSuccess: false };
-  case LOGIN_SUCCESS: return { ...state, user: payload, isLoggedIn: true };
+  case LOGIN_SUCCESS: return {
+    ...state,
+    user: {
+      packageType: packageType(payload.level),
+      ...payload
+    },
+    isLoggedIn: true
+  };
   case LOGIN_FAILED: return { ...state, error: payload };
   case LOGOUT: return { ...state, isLoggedIn: false };
   case UPDATE_USER_PROFILE_IMAGE_SUCCESS: return { ...state, user: { ...state.user, profileImage: payload } };
@@ -51,6 +58,7 @@ export default (state = initialState, { type, payload }) => {
       ...state,
       user: {
         ...state.user,
+        packageType: packageType(payload.level),
         level: payload.level,
       },
       activatedPromoCodes: state.activatedPromoCodes && state.activatedPromoCodes + 1 || 0,
@@ -68,3 +76,14 @@ export default (state = initialState, { type, payload }) => {
   default: return state;
   }
 };
+
+
+function packageType (level) {
+  let type = 'Pro';
+
+  if (level === 4) type = 'Premium';
+
+  if (level >= 6) type = 'Agency';
+
+  return type;
+}
