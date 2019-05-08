5baa954089ab08540f554ca9697eb12f26885d6a
eslam3bed <e.eslam3bed@gmail.com>
2019-05-08 17:10:02 +0300

    removing the ergent  keyword on the label

--- src/components/ProductStepsComponents/Boosters/TermsAndConditions.js
+++ src/components/ProductStepsComponents/Boosters/TermsAndConditions.js
@@ -55,7 +55,7 @@ class ProductTerms extends Component {
           <InputRow margin='45'>
             <InputRow.Label
               name='termsAndConditions'
-              notes='This creates a link to your custom Terms & Conditions page below the Checkout Button on your SamCart checkout page.'
+              notes='This creates a link to your custom Terms & Conditions page below the Checkout Button on your checkout page.'
             >
               Terms & Conditions URL
 
