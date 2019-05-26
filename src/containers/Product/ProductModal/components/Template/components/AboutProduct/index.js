4457890515ed1ea12dc3d3c608c6567b28b9f6c0
eslam3bed <e.eslam3bed@gmail.com>
2019-05-26 05:38:17 +0300

    fix and adust the Product Templte components so they can be reused without issues and in independent manner

--- src/containers/Product/ProductModal/components/Template/components/AboutProduct/index.js
+++ src/containers/Product/ProductModal/components/Template/components/AboutProduct/index.js
@@ -1,41 +1,53 @@
 import React from 'react';
-import defaultLogo from 'assets/images/new-product-icon.png'
-import Image from 'components/common/Image'
-import common from 'components/common'
+import defaultLogo from 'assets/images/new-product-icon.png';
+import Image from 'components/common/Image';
+import common from 'components/common';
 
-import './style.css'
+import './style.css';
 
-const { EditableField } = common
+const { EditableField } = common;
 
 
-const AboutProduct = ({ image = defaultLogo, name, description, onChange }) => {
-  return (
-    <section className="about-product-section underlined">
+const AboutProduct = ({
+  image,
+  name,
+  descriptionInnerClassName = '',
+  containerClassName = '',
+  textAreaClassName = '',
+  subContainerClassName = '',
+  description,
+  onChange
+}) => (
+  <section className={`about-product-section underlined ${containerClassName}`}>
+    {image && (
       <Image
-        image={image}
+        image={image || defaultLogo}
         onChange={(target) => onChange({ target })}
         name='image'
         className='product-template-image'
       />
-      <div className="product-template-description-container">
-        <EditableField
-          name='name'
-          defaultValue='Product Name'
-          onChange={onChange}
-          className="product-template-name"
-          value={name}
-        />
-        <EditableField
-          name='description'
-          defaultValue='Product Description, you can edit it by clicking'
-          onChange={onChange}
-          textarea
-          value={description}
-          className="product-template-description" />
-      </div>
+    )
+    }
+    <div className={`product-template-description-container ${subContainerClassName}`}>
+      <EditableField
+        name='name'
+        defaultValue='Product Name'
+        onChange={onChange}
+        className='product-template-name'
+        value={name}
+      />
+      <EditableField
+        name='description'
+        defaultValue='Product Description, you can edit it by clicking'
+        onChange={onChange}
+        textarea
+        value={description}
+        className={`product-template-description ${descriptionInnerClassName}`}
+      />
+    </div>
 
-    </section>
-  );
-};
+  </section>
+);
+
 
 export default AboutProduct;
--- src/containers/Product/ProductModal/components/Template/components/Testimonials/index.js
+++ src/containers/Product/ProductModal/components/Template/components/Testimonials/index.js
@@ -1,56 +1,59 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Testi as Testimonial } from 'components/Testimonials';
 
-import common from 'components/common'
+import common from 'components/common';
 
-import './style.css'
+import './style.css';
 
-const { EditableField, FloatButton } = common
+const { EditableField, FloatButton } = common;
+const Wrapper = ({ className, children, ...props }) => (className ? <div className={className}>{children}</div> : <Fragment>{children}</Fragment>);
 
 const Testimonials = ({
+  testimonialsWrapperClassName,
+  testimonialClassName,
   testimonials = {},
   ...props
 }) => {
   const { enabled, list = [], title = 'Testimonials' } = testimonials;
 
   const onChange = (name, value) => {
-    const newTestimonials = { ...testimonials, [name]: value }
+    const newTestimonials = { ...testimonials, [name]: value };
     props.onChange({
       target: {
         name: 'checkoutPage.testimonials',
         value: newTestimonials
       }
-    })
-  }
+    });
+  };
 
   const onTestimonialChange = ({ target: { name, value } }) => {
-    const updateMatched = (t, id) => id === name ? value : t;
+    const updateMatched = (t, id) => (id === name ? value : t);
     const newList = list.map(updateMatched);
-    onChange('list', newList)
-  }
+    onChange('list', newList);
+  };
 
   const onDelete = (i) => {
     const newList = list.filter((f, id) => id !== +(i));
-    onChange('list', newList)
-  }
+    onChange('list', newList);
+  };
 
   const onAdd = () => {
-    const newList = [...list, { author: 'edit author name!', content: 'click on text to edit content' }]
-    onChange('list', newList)
-  }
+    const newList = [...list, { author: 'edit author name!', content: 'click on text to edit content' }];
+    onChange('list', newList);
+  };
 
   const onDisable = () => {
-    onChange('enabled', false)
-  }
+    onChange('enabled', false);
+  };
 
   const onTitleChange = ({ target: { value } }) => {
-    onChange('title', value)
-  }
-  if (!enabled) return null
+    onChange('title', value);
+  };
+  if (!enabled) return null;
   return (
     <div className='product-template-testimonials-container'>
-      <FloatButton onClick={onDisable} position={{ left: 0 }} >
-        <i className="fas fa-eye-slash" />
+      <FloatButton onClick={onDisable} position={{ left: 0 }}>
+        <i className='fas fa-eye-slash' />
       </FloatButton>
       <FloatButton
         onClick={onAdd}
@@ -60,8 +63,9 @@ const Testimonials = ({
           top: '0',
           background: 'transparent',
           color: '#4DA1FF'
-        }}>
-        <i className="fas fa-plus-circle" />
+        }}
+      >
+        <i className='fas fa-plus-circle' />
       </FloatButton>
       <h3>
         <EditableField
@@ -71,15 +75,18 @@ const Testimonials = ({
           value={title}
         />
       </h3>
-      {list.map((i, id) => (
-        <Testimonial
-          key={id}
-          id={id}
-          {...i}
-          onDelete={onDelete}
-          onChange={onTestimonialChange}
-        />
-      ))}
+      <Wrapper className={testimonialsWrapperClassName}>
+        {list.map((i, id) => (
+          <Testimonial
+            key={id}
+            id={id}
+            {...i}
+            className={testimonialClassName}
+            onDelete={onDelete}
+            onChange={onTestimonialChange}
+          />
+        ))}
+      </Wrapper>
     </div>
   );
 };
