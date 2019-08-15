757ebec92ff51b6d64a8ed1b0c73d23290378872
eslam3bed <e.eslam3bed@gmail.com>
2019-08-15 17:19:48 +0300

    adding validation schemas and update the data mainpulations flow

--- src/libs/validation/yupMethods.js
+++ src/libs/validation/yupMethods.js
@@ -14,4 +14,11 @@ yup.addMethod(yup.string, 'productUrl', function (key) {
     (value) => !(/([&+,:;=?|'<>.^*()%!-]|\ )/.test(value))
   );
 });
+yup.addMethod(yup.string, 'subDomain', function (key) {
+  return this.test(
+    'subDomain',
+    'invalid subdomain name',
+    (value) => !(/([&+,:;=?|'<>.^*()%!]|\ )/.test(value))
+  );
+});
 
