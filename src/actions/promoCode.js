0943a1d274a7e995e038fcaf7d2be66f549c30a7
eslam3bed <e.eslam3bed@gmail.com>
2019-08-19 23:33:55 +0300

    updating the actions  and reduces for the promocode checking process

--- src/actions/promoCode.js
+++ src/actions/promoCode.js
@@ -3,7 +3,10 @@ import {
   ACTIVATE_AGENCY_CODE,
   ACTIVATE_AGENCY_CODE_SUCCESS,
   ACTIVATE_AGENCY_CODE_FAILED,
-  GET_ACTIVATED_AGENCY_CODES_NUMBERS
+  GET_ACTIVATED_AGENCY_CODES_NUMBERS,
+  CHECK_PROMO_CODE,
+  CHECK_PROMO_CODE_SUCCESS,
+  CHECK_PROMO_CODE_FAILED
 } from '../constantsTypes';
 
 
@@ -16,10 +19,12 @@ export const activateAgencyCode = (code) => ({
   type: ACTIVATE_AGENCY_CODE,
   payload: code
 });
+
 export const activateAgencyCodeSuccess = (message) => ({
   type: ACTIVATE_AGENCY_CODE_SUCCESS,
   payload: message
 });
+
 export const activateAgencyCodeFailed = (message) => ({
   type: ACTIVATE_AGENCY_CODE_FAILED,
   payload: message
@@ -29,3 +34,19 @@ export const getActivatedPromoCodesNumber = (codes) => ({
   type: GET_ACTIVATED_AGENCY_CODES_NUMBERS,
   payload: codes
 });
+
+
+export const checkPromoCode = (promoCode, meta) => ({
+  type: CHECK_PROMO_CODE,
+  payload: promoCode,
+  meta
+});
+export const checkPromoCodeSuccess = (promoCode) => ({
+  type: CHECK_PROMO_CODE_SUCCESS,
+  payload: promoCode
+});
+export const checkPromoCodeFailed = (message) => ({
+  type: CHECK_PROMO_CODE_FAILED,
+  payload: message
+});
+
