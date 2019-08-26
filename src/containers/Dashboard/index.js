65d1c5f26ba992280d4540d7e3c57a595d29301b
eslam3bed <e.eslam3bed@gmail.com>
2019-08-26 11:50:16 +0300

    adding routes changing listener to update and reload userpilot

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Chart from 'components/LeadCartCharts/Chart';
 import { Menu, Link } from 'components/common/MainMenu';
 import InsightBadge from 'components/common/InsightBadge';
@@ -14,64 +14,76 @@ const {
   SubTabs
 } = common;
 
-export default (props) => (
-  <Page>
-    <PageHeader>
-      <MainTitle>Dashboard</MainTitle>
-      <Title>Sales Overview</Title>
-    </PageHeader>
-    <PageContent>
-      <div className='dashboard-page'>
-        <div className='dashboar-content-holder'>
-          <div className='dashboard-sidebar'>
-            <span className='btn btn-primary'>Gross Sales</span>
-            <Menu>
-              <Link>Profit</Link>
-              <Link>LTV</Link>
-              <Link>Refunds</Link>
-              <Link>Orders</Link>
-              <Link>Page Views</Link>
-              <Link>Conversion Rate</Link>
-              <Link>Monthly Recurring Revenue</Link>
-              <Link>Active Subsicriptions</Link>
-              <Link>Churn Rate</Link>
-            </Menu>
-          </div>
-          {/* Chart previews section */}
-          <div className='dashboard-content'>
+export default (props) => {
 
-            <div className='chart-preview section-box'>
-              <div className='chart-head'>
-                <span className='chart-title'>gross sales</span>
-                <span className='chart-total-profite'>
-                  <span className='chart-profit-value'>
-                    $102 387.00
-                  </span>
-                  <span className='stock-up'>17.4%</span>
-                </span>
-              </div>
+  useEffect(() => {
+    const unlisten = props.history.listen((location, action) => {
+      if (window.userpilot)
+        window.userpilot.reload()
+    });
+    return () => {
+    };
+  }, []);
 
-              <div className='chart-body'>
-                <Chart />
-              </div>
+  return (
+    <Page>
+      <PageHeader>
+        <MainTitle>Dashboard</MainTitle>
+        <Title>Sales Overview</Title>
+      </PageHeader>
+      <PageContent>
+        <div className='dashboard-page'>
+          <div className='dashboar-content-holder'>
+            <div className='dashboard-sidebar'>
+              <span className='btn btn-primary'>Gross Sales</span>
+              <Menu>
+                <Link>Profit</Link>
+                <Link>LTV</Link>
+                <Link>Refunds</Link>
+                <Link>Orders</Link>
+                <Link>Page Views</Link>
+                <Link>Conversion Rate</Link>
+                <Link>Monthly Recurring Revenue</Link>
+                <Link>Active Subsicriptions</Link>
+                <Link>Churn Rate</Link>
+              </Menu>
             </div>
+            {/* Chart previews section */}
+            <div className='dashboard-content'>
 
-            {/* overview insights section */}
-            <div className='overview-insights-container'>
-              <InsightBadge title='page views' value={1579} icon={<i className='fas fa-eye' />} />
-              <InsightBadge title='active subscriptions' value={37} icon={<i className='fas fa-user' />} />
-              <InsightBadge title='open payments' value='$4500.00' icon={<i className='fas fa-wallet' />} />
-              <InsightBadge title='open invoices' value={5} icon={<i className='fas fa-file-invoice-dollar' />} />
+              <div className='chart-preview section-box'>
+                <div className='chart-head'>
+                  <span className='chart-title'>gross sales</span>
+                  <span className='chart-total-profite'>
+                    <span className='chart-profit-value'>
+                      $102 387.00
+                  </span>
+                    <span className='stock-up'>17.4%</span>
+                  </span>
+                </div>
+
+                <div className='chart-body'>
+                  <Chart />
+                </div>
+              </div>
+
+              {/* overview insights section */}
+              <div className='overview-insights-container'>
+                <InsightBadge title='page views' value={1579} icon={<i className='fas fa-eye' />} />
+                <InsightBadge title='active subscriptions' value={37} icon={<i className='fas fa-user' />} />
+                <InsightBadge title='open payments' value='$4500.00' icon={<i className='fas fa-wallet' />} />
+                <InsightBadge title='open invoices' value={5} icon={<i className='fas fa-file-invoice-dollar' />} />
+              </div>
             </div>
           </div>
         </div>
+        <div className='dashboard-temp-data-message'>
+          Note: This is dummy data. It will be updated once you have live transactions.
       </div>
-      <div className='dashboard-temp-data-message'>
-        Note: This is dummy data. It will be updated once you have live transactions.
-      </div>
-    </PageContent>
-  </Page>
-);
+      </PageContent>
+    </Page>
+  )
+}
 
 
 /*
