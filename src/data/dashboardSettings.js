8a97929c5ee8b31efa18f28037fa9b96de156626
eslam3bed <e.eslam3bed@gmail.com>
2019-09-11 18:13:35 +0300

    adding the default dashboard setting (available charts settings),adding few constatns actions symbols

---
+++ src/data/dashboardSettings.js
@@ -0,0 +1,32 @@
+
+
+export default {
+  defaultCardsSettings: {
+    sales: [
+      { value: 'dailyRev', label: 'Average Daily Revenue', show: true },
+      { value: 'transactions', label: 'Total Transactions', show: true },
+      { value: 'grossRevenue', label: 'Gross Revenue', show: true },
+      { value: 'netRevenue', label: 'Net Revenue', show: true },
+      { value: 'customers', label: 'Total Customers', show: false },
+      {
+        value: 'newCustomers', label: 'New Customers', disabled: true, show: false
+      },
+      {
+        value: 'annualRevenue', label: 'Annual Revenue', disabled: true, show: false
+      },
+      { value: 'conversionRate', label: 'Conversion Rate', show: true },
+      { value: 'views', label: 'Checkout Views', show: true },
+    ],
+    refunds: [
+      { value: 'refunds', label: 'Refunds', show: true },
+      { value: 'refundRate', label: 'Refund Rate', show: true },
+      {
+        value: 'refundAmount', label: 'Refund Amount', disabled: true, show: false
+      },
+      {
+        value: 'refundedCustomers', label: 'Refunded Customers', disabled: true, show: false
+      },
+    ]
+  },
+  displayMainChart: true
+};
