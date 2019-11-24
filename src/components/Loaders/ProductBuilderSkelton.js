a125e42e0d70ac21e4ceb758556c938c34650a92
eslam3bed <e.eslam3bed@gmail.com>
2019-11-24 18:29:44 +0200

    fixing the produtBuilder and the FUnnel builder loaders

--- src/components/Loaders/ProductBuilderSkelton.js
+++ src/components/Loaders/ProductBuilderSkelton.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './style.css';
 
-const ProductBuilderSkelton = (props) => (
-  <div>
-            ...loading
+const ProductBuilderSkelton = ({ message }) => (
+  <div className='loading-layer'>
+    <div className='loading-message'>{message}</div>
   </div>
 );
 
 ProductBuilderSkelton.propTypes = {
-
+  message: PropTypes.string
+};
+ProductBuilderSkelton.defaultProps = {
+  message: 'Setting Up...'
 };
 
 export default ProductBuilderSkelton;
