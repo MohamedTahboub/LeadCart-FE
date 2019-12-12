162172d0cf8fea251769026d0b82b61e7b03648d
eslam3bed <e.eslam3bed@gmail.com>
2019-12-12 18:11:26 +0200

    handling the data updates and adjusting the languages flow

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,10 +1,15 @@
 export { navigateTo, openNewWindow } from './navigations';
-export { filteringActivities, filterCustomers , RoundTow} from './specialFuncs';
 export { showIntercomIcon } from './intercom';
-export { bytesToSize ,getTextContentFromTextNode} from './conversions';
+export { bytesToSize, getTextContentFromTextNode } from './conversions';
 export { getCurrencySymbol } from './currencies'
 export { default as stopTabClosing } from './stopTabClosing'
 export { default as property } from './propertiesReader'
 export { default as htmlToImage } from './htmlToImage'
 export { default as slugify } from './slugify'
-export *  from './references'
+export * from './references'
+export {
+    filteringActivities,
+    filterCustomers,
+    RoundTow,
+    injectDefaultLabels
+} from './specialFuncs';
