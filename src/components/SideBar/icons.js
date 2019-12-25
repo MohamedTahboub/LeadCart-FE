5804154b12eeb15a88c5664f40790c13b269e3b4
eslam3bed <e.eslam3bed@gmail.com>
2019-12-25 19:24:06 +0200

    resolving the svg as components issue

---
+++ src/components/SideBar/icons.js
@@ -0,0 +1,26 @@
+
+export { ReactComponent as Products } from 'assets/images/icons/products.svg';
+export { ReactComponent as Funnels } from 'assets/images/icons/funnels.svg';
+export { ReactComponent as Fulfillment } from 'assets/images/icons/fulfillments.svg';
+export { ReactComponent as Coupons } from 'assets/images/icons/coupon.svg';
+export { ReactComponent as Transactions } from 'assets/images/icons/transactions.svg';
+export { ReactComponent as Customers } from 'assets/images/icons/customers.svg';
+export { ReactComponent as Affiliates } from 'assets/images/icons/affiliates.svg';
+export { ReactComponent as SubAccounts } from 'assets/images/icons/subAccounts.svg';
+export { ReactComponent as Settings } from 'assets/images/icons/settings.svg';
+export { ReactComponent as Help } from 'assets/images/icons/help.svg';
+
+
+// export default {
+//   Products,
+//   Funnels,
+//   Fulfillment,
+//   Coupons,
+//   Transactions,
+//   Customers,
+//   Affiliates,
+//   SubAccounts,
+//   Settings,
+//   Help
+// };
+
