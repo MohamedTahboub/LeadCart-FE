b9ee72cfb0b69538ed40348204770b89d8356c7d
eslam3bed <e.eslam3bed@gmail.com>
2020-01-16 15:50:32 +0200

    add default data, and create helping functions for wide uses

---
+++ src/data/sampleRules.js
@@ -0,0 +1,29 @@
+export default [{
+  trigger: 'Purchase Complete',
+  triggerGroups: [
+    {
+      products: [
+        '5e0e30c1441a9a28244bd83a'
+      ],
+      action: {
+        serviceName: 'Aweber',
+        serviceAction: 'Add to Welcome List'
+      }
+    },
+    {
+      products: ['5e0e30c1441a9a28244bd83a', '5e085c981c2711255205cff4'],
+      action: {
+        serviceName: 'Aweber',
+        serviceAction: 'Add to Welcome List'
+      }
+    },
+    {
+      products: ['5e0853781c2711255205cff2', '5e0e30c1441a9a28244bd83a'],
+      action: {
+        serviceName: 'Aweber',
+        serviceAction: 'Add to Welcome List'
+      }
+    }
+  ]
+}];
+
