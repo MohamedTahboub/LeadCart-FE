ab62d12c04ce3599ec19135b50b6b02dcb26c21a
eslam3bed <e.eslam3bed@gmail.com>
2020-05-29 14:53:43 +0300

    [update]: update legacy links to the new s3 ref

--- src/libs/validation/funnelSchema.js
+++ src/libs/validation/funnelSchema.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 import castYupErrors from './castErrors';
 
-const funnelCoverDefaultImage = 'https://s3.us-east-2.amazonaws.com/static.leadcart.io/5d3bd34e97d3ea503e8659af/products/funnelDemoFlow.png';
+const funnelCoverDefaultImage = 'https://s3.us-east-2.amazonaws.com/assets.leadcart.io/5d3bd34e97d3ea503e8659af/products/funnelDemoFlow.png';
 
 const coordinatesSchema = yup.object({
   x: yup.number(),
