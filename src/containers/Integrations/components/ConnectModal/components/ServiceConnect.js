f63b7f44e59006d6ef1dffb0fc1f166b185e90bc
eslam3bed <e.eslam3bed@gmail.com>
2020-06-07 22:14:01 +0300

    [UrgentFix]: fixing few issues on production

--- src/containers/Integrations/components/ConnectModal/components/ServiceConnect.js
+++ src/containers/Integrations/components/ConnectModal/components/ServiceConnect.js
@@ -137,7 +137,7 @@ const ConnectIntegration = ({ authType, onConnect, onModalToggle, ...props }) =>
   return (
     <LayoutSwitch active={authType}>
       <ConnectOAuth id='OAuth' {...props} />
-      <ConnectClient id='clientCredentials' {...props} {...customProps} />
+      <ConnectClient id='client_credentials' {...props} {...customProps} />
       <ConnectApiKey id='apiKey' {...props} {...customProps} />
     </LayoutSwitch>
   );
