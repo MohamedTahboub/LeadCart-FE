32d44cd534eb21d1158c7ee27b67b1c34e6777da
fares98 <faresalhello21@gmail.com>
2020-06-11 01:47:25 +0300

    edit the import after edit the TestUtils

--- src/containers/Login/login.test.js
+++ src/containers/Login/login.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import render from 'components/TestUtils';
+import { render } from 'components/TestUtils';
 import Login from './index';
 import '@testing-library/jest-dom/extend-expect';
 
