6389b489eb61e0f1d873fec59f6d28ee5c5638ba
fares98 <faresalhello21@gmail.com>
2020-06-28 20:38:56 +0300

    create file for the needed data for the builder section

---
+++ src/containers/ProductPageBuilder/components/SideBar/data.js
@@ -0,0 +1,32 @@
+import headingText from 'assets/images/product_builder_icons/headingText.png';
+import button from 'assets/images/product_builder_icons/button.png';
+import bumpOffer from 'assets/images/product_builder_icons/bumpOffer.png';
+import video from 'assets/images/product_builder_icons/video.png';
+import gurantee from 'assets/images/product_builder_icons/gurantee.png';
+import features from 'assets/images/product_builder_icons/features.png';
+import figure from 'assets/images/product_builder_icons/figure.png';
+import text from 'assets/images/product_builder_icons/text.png';
+import countdown from 'assets/images/product_builder_icons/countdown.png';
+import image from 'assets/images/product_builder_icons/image.png';
+import embedCode from 'assets/images/product_builder_icons/embedCode.png';
+import progressBar from 'assets/images/product_builder_icons/progressBar.png';
+import testimonials from 'assets/images/product_builder_icons/testimonials.png';
+import spacer from 'assets/images/product_builder_icons/spacer.png';
+
+export const data = [
+  { type: 'headingText', title: 'Heading Text', src: headingText },
+  { type: 'text', title: 'Text', src: text },
+  { type: 'button', title: 'Button', src: button },
+  { type: 'image', title: 'Image', src: image },
+  { type: 'video', title: 'Video', src: video },
+  { type: 'spacer', title: 'Spacer', src: spacer },
+  { type: 'gurantee', title: 'Gurantee', src: gurantee },
+  { type: 'countdown', title: 'Countdown', src: countdown },
+  { type: 'progressBar', title: 'Progree Bar', src: progressBar },
+  { type: 'figure', title: 'Figure', src: figure },
+  { type: 'testimonials', title: 'Testimonials', src: testimonials },
+  { type: 'features', title: 'Features', src: features },
+  { type: 'bumpOffer', title: 'Bump Offer', src: bumpOffer },
+  { type: 'embedCode', title: 'Embed Code', src: embedCode }
+];
+
