cca5ebdae49c8a9a7ba9de34187235ac8514a732
NoureldeanSaed <Noureldean.saed@gmail.com>
2020-07-05 14:29:09 +0300

    Modifying product refund reducer

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -11,26 +11,59 @@ export default (state = initState, { type, payload }) => {
       ...state,
       list: payload
     };
-  case ORDER_REFUND_SUCCESS:
+  case ORDER_REFUND_SUCCESS: {
+    const oldOrders = payload.orders;
+    const orders = oldOrders.map((order) => {
+      if (order._id === payload.orderId) {
+        let totalCharge = 0;
+        // Go through order.products to update the refunded product
+        const products = order.products.map((product) => {
+          if (payload.productId !== product.id) {
+            totalCharge += product.price;
+            return product;
+          }
+          let price = product.price;
+          if (payload.target === 'offer') {
+            // Go through order.product.offers to update the refunded product's offer
+            const offers = product.offers.map((offer) => {
+              if (offer.id === payload.productId) {
+                price -= offer.price;
+                totalCharge += price;
+                return { ...offer, refunded: true, price: -offer.price };
+              }
+              return offer;
+            });
+            totalCharge += price;
+            return { ...product, offers, price };
+          } else if (payload.target === 'product') {
+            return {
+              ...product,
+              payment: { ...product.payment, paymentRefunded: true },
+              price: 0
+            };
+          }
+          return product;
+        });
+        return { ...order, products, totalCharge };
+      }
+      return order;
+    });
     return {
       ...state,
       list: state.list.map((customer) => {
         const refundedOrder = customer.orders.find((order) => order === payload.orderId);
         if (refundedOrder) {
-          const lifeTimeCharges = payload.orders.reduce((lifeTimeCharges, order) => {
+          const lifeTimeCharges = orders.reduce((lifeTimeCharges, order) => {
             if (order.customer.email === customer.email)
               return lifeTimeCharges + order.totalCharge;
             return lifeTimeCharges;
           }, 0).toString();
-          return {
-            ...customer,
-            lifeTimeCharges
-          };
+          return { ...customer, lifeTimeCharges };
         }
-
         return customer;
       })
     };
+  }
   default: return state;
   }
 };
--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -12,27 +12,39 @@ export default (state = initState, { type, payload }) => {
     return payload;
 
   case ORDER_REFUND_SUCCESS: {
-
     // Go through orders to update the refunded order
     const orders = state.map((order) => {
       if (order._id === payload.orderId) {
+        let totalCharge = 0;
         // Go through order.products to update the refunded product
         const products = order.products.map((product) => {
+          if (payload.productId !== product.id) {
+            totalCharge += product.price;
+            return product;
+          }
           let price = product.price;
           if (payload.target === 'offer') {
             // Go through order.product.offers to update the refunded product's offer
             const offers = product.offers.map((offer) => {
               if (offer.id === payload.productId) {
                 price -= offer.price;
+                totalCharge += price;
                 return { ...offer, refunded: true, price: -offer.price };
               }
               return offer;
             });
+            totalCharge += price;
             return { ...product, offers, price };
+          } else if (payload.target === 'product') {
+            return {
+              ...product,
+              payment: { ...product.payment, paymentRefunded: true },
+              price: 0
+            };
           }
-          return { ...product, price };
+          return product;
         });
-        return { ...order, products };
+        return { ...order, products, totalCharge };
       }
       return order;
     });
