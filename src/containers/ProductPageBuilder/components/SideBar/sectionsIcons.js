b6f3f38b7a8e7b761c55a1b5d820dbadf3194794
Eslam A. Hugair <e.eslam3bed@gmail.com>
2020-07-06 13:17:25 +0300

    Product builder icons type map issues (#400)
    * [fix]: fixing typos and sections type changes casing sections miss match bugs
    
    * [fix]: flag the TextField input as uncontrolled :bug:

--- src/containers/ProductPageBuilder/components/SideBar/sectionsIcons.js
+++ src/containers/ProductPageBuilder/components/SideBar/sectionsIcons.js
@@ -2,7 +2,7 @@ import headingText from 'assets/images/product_builder_icons/headingText.png';
 import button from 'assets/images/product_builder_icons/button.png';
 import bumpOffer from 'assets/images/product_builder_icons/bumpOffer.png';
 import video from 'assets/images/product_builder_icons/video.png';
-import gurantee from 'assets/images/product_builder_icons/gurantee.png';
+import guarantee from 'assets/images/product_builder_icons/gurantee.png';
 import features from 'assets/images/product_builder_icons/features.png';
 import figure from 'assets/images/product_builder_icons/figure.png';
 import text from 'assets/images/product_builder_icons/text.png';
@@ -14,20 +14,20 @@ import testimonials from 'assets/images/product_builder_icons/testimonials.png';
 import spacer from 'assets/images/product_builder_icons/spacer.png';
 
 const sectionsIcons = [
-  { type: 'headingText', title: 'Heading Text', src: headingText },
+  { type: 'heading', title: 'Heading Text', src: headingText },
   { type: 'text', title: 'Text', src: text },
   { type: 'button', title: 'Button', src: button },
   { type: 'image', title: 'Image', src: image },
   { type: 'video', title: 'Video', src: video },
   { type: 'spacer', title: 'Spacer', src: spacer },
-  { type: 'gurantee', title: 'Gurantee', src: gurantee },
-  { type: 'countdown', title: 'Countdown', src: countdown },
-  { type: 'progressBar', title: 'Progree Bar', src: progressBar },
+  { type: 'guaranteeWidget', title: 'Guarantee', src: guarantee },
+  { type: 'countDownWidget', title: 'Countdown', src: countdown },
+  { type: 'progressbarWidget', title: 'Progress Bar', src: progressBar },
   { type: 'figure', title: 'Figure', src: figure },
-  { type: 'testimonials', title: 'Testimonials', src: testimonials },
-  { type: 'features', title: 'Features', src: features },
+  { type: 'testimonialsSection', title: 'Testimonials', src: testimonials },
+  { type: 'featuresSection', title: 'Features', src: features },
   { type: 'bumpOffer', title: 'Bump Offer', src: bumpOffer },
-  { type: 'embedCode', title: 'Embed Code', src: embedCode }
+  { type: 'code', title: 'Embed Code', src: embedCode }
 ];
 
 export default sectionsIcons;
