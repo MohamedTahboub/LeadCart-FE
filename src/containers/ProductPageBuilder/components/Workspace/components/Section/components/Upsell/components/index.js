4fd868012ece3eb5aa5796e426b737c6e7a4b9d8
fares98 <faresalhello21@gmail.com>
2020-07-25 15:59:23 +0300

    solve bug

--- src/containers/ProductPageBuilder/components/Workspace/components/Section/components/Upsell/components/index.js
+++ src/containers/ProductPageBuilder/components/Workspace/components/Section/components/Upsell/components/index.js
@@ -1,7 +1,1 @@
-export { default as BillingDetails } from './BillingDetails';
 export { default as CompleteOrderBtn } from './CompleteOrderBtn';
-export { default as OrderSummary } from './OrderSummary';
-export { default as PaymentMethods } from './PaymentMethods';
-export { default as ShippingDetails } from './ShippingDetails';
-export { default as CouponSection } from './CouponSection';
-export { default as Receipt } from './Receipt';
