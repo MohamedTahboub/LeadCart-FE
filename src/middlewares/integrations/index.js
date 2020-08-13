48db5a24bb2c40b4c6c9b5de1a109fd7040ce47d
eslam3bed <e.eslam3bed@gmail.com>
2020-08-13 19:04:36 +0300

    [update]: adding actions and middlware to handle the webhook test ajax

--- src/middlewares/integrations/index.js
+++ src/middlewares/integrations/index.js
@@ -2,5 +2,6 @@ import connect from './connect';
 import disconnect from './disconnect';
 import check from './check';
 import actionRequirements from './actionRequirements';
+import webhookTest from './webhookTest';
 
-export default [connect, disconnect, check, actionRequirements];
+export default [connect, disconnect, check, actionRequirements, webhookTest];
