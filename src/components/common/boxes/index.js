a7fe0d3875d825dafbf4233b4c1c943887ca4422
eslam3bed <e.eslam3bed@gmail.com>
2020-08-17 14:22:22 +0300

    [fix]: fix and adjust the funnel embeding issue, and the Button script styles updates

--- src/components/common/boxes/index.js
+++ src/components/common/boxes/index.js
@@ -1,7 +1,7 @@
 import Block from './Block';
 import Box from './Box';
 import CardsContainer from './CardsContainer';
-import EmbededScripContainer from './EmbededScripContainer';
+import EmbeddedScripContainer from './EmbeddedScripContainer';
 import FlexBox from './FlexBox';
 import FlexBoxesContainer from './FlexBoxesContainer';
 import InsightBadge from './InsightBadge';
@@ -10,4 +10,4 @@ import SmallBox from './SmallBox';
 import './style.css';
 
 
-export { Block, Box, CardsContainer, EmbededScripContainer, FlexBox, FlexBoxesContainer, InsightBadge, MainBlock, SmallBox };
+export { Block, Box, CardsContainer, EmbeddedScripContainer, FlexBox, FlexBoxesContainer, InsightBadge, MainBlock, SmallBox };
