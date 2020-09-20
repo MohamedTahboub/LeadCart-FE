3043f26f108f3985ec2b9f95ed034958d124a280
eslam3bed <e.eslam3bed@gmail.com>
2020-09-20 13:37:57 +0300

    [update]: enable order refund trigger event, and the revoke action for users (just for admins)

--- src/containers/FunnelBuilder/components/Rules/components/RuleModal/ActionDependencies/components/index.js
+++ src/containers/FunnelBuilder/components/Rules/components/RuleModal/ActionDependencies/components/index.js
@@ -4,3 +4,4 @@ export { default as ManualFulfillment } from './ManualFulfillment';
 export { default as ExternalIntegration } from './ExternalIntegration';
 export { default as PrivateLeadcartFulfillment } from './PrivateLeadcartFulfillment';
 export { default as WebhooksForm } from './WebhooksForm';
+export { default as RevokeAccessFulfillment } from './RevokeAccessFulfillment';
