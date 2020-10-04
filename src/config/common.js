36ad1493772c0b1eefef02f728ebd7ae350200b3
eslam3bed <e.eslam3bed@gmail.com>
2020-10-04 18:22:15 +0300

    [fix]: removing the s from domains

--- src/config/common.js
+++ src/config/common.js
@@ -56,7 +56,7 @@ export default {
         {
           key: 'Assign Roles & Permissions To Team Members (Coming Soon).',
           list: [
-            'Custom Domains (CNAME).',
+            'Custom Domain (CNAME).',
             'Checkout & Funnels',
             'Emails'
           ]
