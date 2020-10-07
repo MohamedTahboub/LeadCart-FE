d66265775450d157442161702060430450a2bba3
eslam3bed <e.eslam3bed@gmail.com>
2020-10-07 14:36:51 +0300

    [fix]: edge case for the product offer

--- src/containers/FunnelBuilder/components/Rules/components/helpers.js
+++ src/containers/FunnelBuilder/components/Rules/components/helpers.js
@@ -56,7 +56,7 @@ export const getTriggerLabel = (val) => {
 export const getProductsPricingOptions = (products = [], globalProductsMap = {}) => {
   return products
     .map((p) => p.value ? globalProductsMap[p.value] : globalProductsMap[p])
-    .filter((product) => (Array.isArray(product.pricingOptions) && product.pricingOptions.length))
+    .filter((product = {}) => (Array.isArray(product.pricingOptions) && product.pricingOptions.length))
     .map((product) => product.pricingOptions)
     .flat()
     .map(({ label, id: value }) => ({ label, value }));
