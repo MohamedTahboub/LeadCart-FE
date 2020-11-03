2cee141bc5274ba7c77b53b52a4aa4ba318122c6
eslam3bed <e.eslam3bed@gmail.com>
2020-11-03 16:18:09 +0200

    [update]: adding the templates to the builder sidebar

--- src/containers/ProductPageBuilder/components/SideBar/index.js
+++ src/containers/ProductPageBuilder/components/SideBar/index.js
@@ -1,33 +1,44 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FaHome } from 'react-icons/fa';
+import { BsLayoutWtf } from 'react-icons/bs';
 
 import common from 'components/common';
 import { BaseCard } from './components';
 import sectionsIcons from './sectionsIcons';
 import './style.css';
+import { TemplatesList } from '../PageSetupModal/components';
 
-const { SideMenu, Tabs, Tab } = common;
+const { SideMenu, Tabs, Tab, FlexBox } = common;
+
+const SideBarTitle = ({ title, icon }) => {
+  return (
+    <div className='builder-section__header'>
+      <span className='builder-section__header-icon' >{icon}</span>
+      <span className='builder-side-bar-title'>{title}</span>
+    </div>
+  );
+};
 
 const SideBar = ({ canOffer }) => {
   const [isToggled, setIsToggled] = useState(false);
+  const [selectedTemplate, setSelectedTemplate] = useState();
 
   const sideBarData = canOffer ? sectionsIcons : sectionsIcons.filter((ele) => ele.type !== 'bumpOffer');
-  const builderSectionTitle =
-    (<div className='builder-section__header'>
-      <span className='builder-section__header-icon' ><FaHome /></span>
-      Builder Section
-    </div>);
 
 
   const onToggle = () => {
     setIsToggled((open) => !open);
   };
 
+  const onTemplateSelect = (templateId) => () => {
+    setSelectedTemplate(templateId);
+  };
+
   return (
     <SideMenu open={isToggled} onToggle={onToggle} toggleOnHover>
       <Tabs active='builderSection'>
-        <Tab id='builderSection' title={builderSectionTitle}>
+        <Tab id='builderSection' title={<SideBarTitle title='Builder Section' icon={<FaHome />} />}>
           <section className='builder-section'>
             {
               sideBarData.map((section) => (
@@ -38,6 +49,15 @@ const SideBar = ({ canOffer }) => {
               ))
             }
           </section>
+        </Tab>
+        <Tab id='templates' title={<SideBarTitle title='Templates' icon={<BsLayoutWtf />} />}>
+          <FlexBox column className='side-bar-templates-list'>
+            <TemplatesList
+              onSelect={onTemplateSelect}
+              cardClassName='template-card-for-side-bar'
+              selected={selectedTemplate}
+            />
+          </FlexBox>
         </Tab>
       </Tabs>
     </SideMenu>
