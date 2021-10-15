e109a165417ccd85ec8b4c6c1f21e79002d40314
Eslam <eslam@leadcart.io>
2021-10-15 06:40:01 +0300

    [update]: moving sorting to the backend

--- src/reducers/prospects.js
+++ src/reducers/prospects.js
@@ -3,29 +3,13 @@ import { GET_BRAND_PROSPECTS_SUCCESS } from '../constantsTypes';
 
 const initialState = [];
 
-export const filterProspects = (prospects) => {
-  if (!(Array.isArray(prospects) && prospects.length > 1)) return [];
-
-
-  return Object.values(prospects.reduce((list, prospect) => {
-    const { email } = prospect;
-    if (list[email]) {
-      list[email].history.push(prospect);
-    } else {
-      list[email] = { ...prospect };
-      if (!list[email].history) list[email].history = [prospect];
-    }
-    return list;
-  }, {}));
-
-};
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
   case GET_BRAND_PROSPECTS_SUCCESS:
 
 
-    return filterProspects(payload) || [];
+    return payload || [];
   default: return state;
   }
 };
